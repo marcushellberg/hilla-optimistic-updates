e494cc18141fe3bf948d91158b5d188ee1a2aafd
start.vaadin.com <no-reply@vaadin.com>
2022-10-14 22:42:20 +0000

    Generated project

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,25 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "hilla-optimistic-updates")
+@PWA(name = "hilla-optimistic-updates", shortName = "hilla-optimistic-updates", offlineResources = {})
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
